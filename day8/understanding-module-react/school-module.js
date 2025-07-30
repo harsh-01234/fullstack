48df968aca88cf6064bcbe792ca306aba8893ad3
HARSH <harsh10a2005@gmail.com>
2025-07-30 15:50:53 +0530

    Delete day8 directory

--- day8/understanding-module-react/school-module.js
+++
@@ -1,9 +0,0 @@
-let myFees=0;
-function payFees(payment){
-    console.log(`your fees ${payment} is paid`);
-    myFees=payment;
-}
-function getFees(){
-    return myFees;
-}
-export{payFees,getFees};
